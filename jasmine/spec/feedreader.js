2b5936452408605341ad68fe9b9f3263b7123d25
Juan Moreno <44890837+juanManGit@users.noreply.github.com>
2019-07-05 17:30:24 -0500

    Updated some comments
    Just capitalized a few comments and added a couple more.

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -69,22 +69,22 @@ $(function() {
             
                 it('Menu toggles', function(){
                     
-                    //stores menu element in a variable
+                    //Stores menu element in a variable
                 
                     let menuIcon = $('.menu-icon-link');
                     
-                    //recreates a click event
+                    //Recreates a click event
                     
                     menuIcon.click();
                     
-                    //checks if the menu/hidden class is no longer present.
+                    //Checks if the menu/hidden class is no longer present.
                     expect($('body').hasClass('menu-hidden')).toBe(false);
                     
-                    //recreates another click event
+                    //Recreates another click event
 
                     menuIcon.click();
                     
-                    //checks if the menu-hidden class is present again
+                    //Checks if the menu-hidden class is present again
                     expect($('body').hasClass('menu-hidden')).toBe(true);
                     
                     
@@ -98,14 +98,14 @@ $(function() {
 
         //Initial entries tests
         describe('Initial Entries', function() {
-                //ensures that loadFeed is done running
+                //Ensures that loadFeed is done running
                 beforeEach(function(done){
                     
                 loadFeed(0,done);
                     
                 });
                 
-            
+                //Tests if loadFeed has at least 1 entry
                 it('loadFeed is called and completes work', function(){
                     //gets the number of entries from feed
                     const feedLen = document.querySelector('.feed').children.length; 
@@ -122,17 +122,17 @@ $(function() {
     
         //Loads a different feed
         describe('New Feed Selection', function(){
-        //sets a couple of veriables in the scope of the suite so that they can be set and accessed by before beforeEach() and it(). It sets them to null so that we can test if the original value changed.
+        //Sets a couple of variables in the scope of the suite so that they can be set and accessed by before beforeEach() and it(). It sets them to null so that we can test if the original value changed.
             let initialFeed = null;
             let currentFeed = null;
 
-                //ensures that loadFeed has been successfully finished before executing the tests
+                //Ensures that loadFeed has been successfully finished before executing the tests
                 beforeEach(function(done) {
-                        //it loads the first feed and gets the text out of it. Then converts it to a string and gets a unique number from it
+                        //It loads the first feed and gets the text out of it. Then converts it to a string and gets a unique number from it
                         loadFeed(0);
                     
                         initialFeed = hashString($('.feed').text());
-                        //it loads a second feed and finalizes the execution of beforeEach().
+                        //It loads a second feed and finalizes the execution of beforeEach().
                         loadFeed(1,done);                                    
                 });
             
@@ -141,14 +141,14 @@ $(function() {
                 it('Feed is loaded and refreshes', function(){
                     
        
-                    //gets a different feed, also convert it to a string and then converts it into a unique hash number.
+                    //Gets a different feed, also converts it to a string and then converts it into a unique hash number.
                     currentFeed = hashString($('.feed').text());
                                         
-                    //ensures that the original value of initialFeed and currentFeed have been reassigned to the hashes.
+                    //Ensures that the original value of initialFeed and currentFeed have been reassigned to the hashes.
                     expect(initialFeed).not.toEqual(null);
                     expect(currentFeed).not.toEqual(null);
                     
-                    //compares the two hashes. If they're different it means the feed loaded a different set of articles
+                    //Compares the two hashes. If they're different it means the feed loaded a different set of articles
                     expect(initialFeed).not.toEqual(currentFeed);
                     
                 })
